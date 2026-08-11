1ae5ca7e1f54786808bd5e31506b89f4f49313d2
bolaedward <bolaedward29@gmail.com>
2026-08-11 12:43:05 +0300

    Implement generic repository

---
+++ ECommerce.Domain/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(Guid id, CancellationToken ct = default);
+
+        Task<IReadOnlyList<T>> GetAllAsync(CancellationToken ct = default);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ ECommerce.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,42 @@
+﻿using ECommerce.Domain.Repositories;
+using ECommerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using ECommerce.Domain.Entities;
+
+namespace ECommerce.Infrastructure.Repositories
+{
+    public sealed class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ECommerceDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ECommerceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id, CancellationToken ct = default)
+        {
+            return await _dbSet.FirstOrDefaultAsync(entity => entity.Id == id, ct);
+        }
+        public async Task<IReadOnlyList<T>> GetAllAsync(CancellationToken ct = default)
+        {
+            return await _dbSet.AsNoTracking().ToListAsync(ct);
+        }
+
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+        public void Delete(T entity)
+        {
+            entity.MarkAsDeleted();
+        }
+    }
+}
